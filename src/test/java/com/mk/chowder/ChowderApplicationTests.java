a25dad725023e48503af7b57a4c64987fe85c50e
zhy <2441373260@qq.com>
2022-07-22 16:22:09 +0800

    Initial commit

---
+++ src/test/java/com/mk/chowder/ChowderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mk.chowder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChowderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
